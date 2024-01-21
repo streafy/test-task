c1313a9bb0ce00a663a802f2bbd69c1d82cd8958
streafy <streafyhs@gmail.com>
2024-01-21 17:17:50 +0700

    add top bar

--- app/src/main/java/com/streafy/test_task/ui/MainScreen.kt
+++ app/src/main/java/com/streafy/test_task/ui/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.streafy.test_task.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -10,9 +12,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.rememberNavController
 import com.streafy.test_task.navigation.AppNavHost
@@ -33,23 +37,32 @@ fun MainScreen() {
 
             val navigationItems = listOf(NavigationItem.Cameras, NavigationItem.Doors)
 
-            TabRow(selectedTabIndex = state) {
-                navigationItems.forEachIndexed { index, item ->
-                    val selected = navBackStackEntry?.destination?.hierarchy?.any {
-                        it.route == item.screen.route
-                    } ?: false
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Мой дом",
+                    modifier = Modifier.padding(14.dp),
+                    fontSize = 21.sp
+                )
+                TabRow(selectedTabIndex = state) {
+                    navigationItems.forEachIndexed { index, item ->
+                        val selected = navBackStackEntry?.destination?.hierarchy?.any {
+                            it.route == item.screen.route
+                        } ?: false
 
-                    Tab(
-                        selected = selected,
-                        onClick = {
-                            if (!selected) {
-                                state = index
-                                navController.navigate(item.screen.route)
-                            }
-                        },
-                        modifier = Modifier.height(44.dp)
-                    ) {
-                        Text(text = item.name)
+                        Tab(
+                            selected = selected,
+                            onClick = {
+                                if (!selected) {
+                                    state = index
+                                    navController.navigate(item.screen.route)
+                                }
+                            },
+                            modifier = Modifier.height(44.dp)
+                        ) {
+                            Text(text = item.name)
+                        }
                     }
                 }
             }
